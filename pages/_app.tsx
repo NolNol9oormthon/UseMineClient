0843aaedd5f3e274bc0215bcfda0965667f3a868
hyunju <dlguswn3659@github.com>
2022-12-14 23:40:48 +0900

    [init] firebase deploy reset and reinit / set maxwidth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,34 @@
 import GlobalStyle from '../src/styles/globalStyle';
 import type { AppProps } from 'next/app';
+import styled from 'styled-components';
+
+const BodyInner = styled.div`
+  display: flex;
+  // flex-direction: column;
+  justify-content: center;
+  // align-items: center;
+  min-height: 100vh;
+  background-color: #000000;
+`;
+
+const WebAppContainer = styled.div`
+  width: 100%;
+  max-width: 375px;
+  height: 100%;
+  min-height: 100vh;
+  background-color: #ffffff;
+  // position: absolute;
+`;
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <>
       <GlobalStyle />
-      <Component {...pageProps} />
+      <BodyInner>
+        <WebAppContainer>
+          <Component {...pageProps} />
+        </WebAppContainer>
+      </BodyInner>
     </>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
 import { NextPage } from 'next';
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 100%;
+`
 
 const Home: NextPage = () => {
-  return <div>안녕하세요 놀놀팀입니다.</div>;
+  return <Container>안녕하세요 놀놀팀입니다.</Container>;
 };
 
 export default Home;
