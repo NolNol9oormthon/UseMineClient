121d3aeecfec3fbca02a6d8bb2f75a6d798df467
hyunju <dlguswn3659@github.com>
2022-12-15 05:39:13 +0900

    [add] kakao login done

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,11 @@
 import GlobalStyle from '../src/styles/globalStyle';
+import { useEffect } from 'react';
 import type { AppProps } from 'next/app';
 import styled, { ThemeProvider } from 'styled-components';
 import { theme } from '../src/styles/theme';
-import "../src/font/font.css";
+import '../src/font/font.css';
+import Script from 'next/script';
+
 
 const BodyInner = styled.div`
   display: flex;
@@ -10,7 +13,7 @@ const BodyInner = styled.div`
   justify-content: center;
   // align-items: center;
   min-height: 100vh;
-  background-color: ${props => props.theme.colors.gray100};
+  background-color: ${(props) => props.theme.colors.gray100};
 `;
 
 const WebAppContainer = styled.div`
@@ -19,11 +22,23 @@ const WebAppContainer = styled.div`
   height: 100%;
   min-height: 100vh;
   padding: 0px 20px;
-  background-color: ${props => props.theme.colors.white};
+  background-color: ${(props) => props.theme.colors.white};
   // position: absolute;
 `;
 
+declare global {
+  interface Window {
+    Kakao: any;
+  }
+}
+
 const App = ({ Component, pageProps }: AppProps) => {
+  function kakaoInit() {
+    // 페이지가 로드되면 실행
+    window.Kakao.init(process.env.NEXT_PUBLIC_KAKAO_JS_API_KEY);
+    console.log(window.Kakao.isInitialized());
+  }
+
   return (
     <>
       <ThemeProvider theme={theme}>
@@ -31,6 +46,7 @@ const App = ({ Component, pageProps }: AppProps) => {
         <BodyInner>
           <WebAppContainer>
             <Component {...pageProps} />
+            <Script src="https://developers.kakao.com/sdk/js/kakao.js" onLoad={kakaoInit}></Script>
           </WebAppContainer>
         </BodyInner>
       </ThemeProvider>
---
+++ pages/welcome.tsx
@@ -0,0 +1,194 @@
+import React, { useState, useEffect } from 'react';
+import { NextPage } from 'next';
+import { useRouter } from 'next/router';
+import styled from 'styled-components';
+import axios from 'axios';
+import qs from 'qs';
+import { useNavigate } from 'react-router-dom';
+import Image from 'next/image';
+import { ChevronRightBlue, ChevronRightOrange } from '../assets/icons';
+import animation from '../assets/lottie/airplane_loading.json';
+import Lottie from 'reactjs-lottie';
+
+const Container = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  height: 100%;
+  font-family: NanumSquare Neo variable;
+  font-size: 30px;
+  font-weight: 700;
+  position: relative;
+`;
+
+const MainText = styled.div`
+  font-family: NanumSquare Neo variable;
+  font-size: 24px;
+  font-weight: 500;
+  line-height: 32px;
+  letter-spacing: 0em;
+  text-align: left;
+  padding-top: 90px;
+`;
+
+const SemiText = styled.div`
+  font-family: NanumSquare Neo variable;
+  font-size: 16px;
+  font-weight: 300;
+  line-height: 28px;
+  text-align: left;
+  margin-top: 2px;
+  margin-bottom: 53px;
+`;
+
+const Button = `
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  height: 56px;
+  border-radius: 8px;
+  padding: 8px 28px;
+  text-align: left;
+  margin-bottom: 16px;
+  align-items: center;
+`;
+
+const RegisterButton = styled.button`
+  ${Button}
+  background-color: ${(props) => props.theme.colors.tam_blue200};
+`;
+
+const ViewButton = styled.button`
+  ${Button}
+  background-color: ${(props) => props.theme.colors.tam_Orange100};
+`;
+
+const ButtonText = `
+  font-weight: 700;
+  font-family: NanumSquare Neo variable;
+  font-size: 16px;
+`;
+
+const ButtonTextBlue = styled.div`
+  ${ButtonText}
+  color: ${(props) => props.theme.colors.tam_blue500};
+`;
+
+const ButtonTextOrange = styled.div`
+  ${ButtonText}
+  color: ${(props) => props.theme.colors.tam_Orange500};
+`;
+
+const ButtonIcon = styled.div`
+  width: 24px;
+  height: 24px;
+`;
+
+const LottieContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  padding-top: 100px;
+`
+
+const Home: NextPage = () => {
+  const [userInfo, setUserInfo] = useState({});
+  // const [code, setCode] = useState('');
+  const router = useRouter();
+  // const history = useNavigate();
+
+  useEffect(() => {
+    console.log(router.query['code']);
+    var code = router.query['code'];
+
+    const getProfile = async () => {
+      try {
+        // Kakao SDK API를 이용해 사용자 정보 획득
+        let data = await window.Kakao.API.request({
+          url: '/v2/user/me',
+        });
+        // 사용자 정보 변수에 저장
+        console.log(data);
+        if (data.id) {
+          const sendData = {
+            id: data.id,
+            profileUrl: data.properties.profile_image,
+          };
+          // 백에 보내
+          const returnData = {
+            id: 'test3',
+            nickname: '곤란한 한라봉',
+          };
+          setUserInfo(returnData);
+        }
+        // setUserId(data.id);
+        // setNickName(data.properties.nickname);
+        // setProfileImage(data.properties.profile_image);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    const getToken = async () => {
+      const payload = qs.stringify({
+        grant_type: 'authorization_code',
+        client_id: process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY,
+        redirect_uri: 'http://localhost:3000/welcome',
+        code: code,
+        client_secret: process.env.NEXT_PUBLIC_KAKAO_CLIENT_SECRET,
+      });
+      try {
+        // access token 가져오기
+        const res = await axios.post('https://kauth.kakao.com/oauth/token', payload);
+
+        // access token 설정
+        window.Kakao.Auth.setAccessToken(res.data.access_token);
+        // history.replace('/profile');
+        getProfile();
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    if (code !== undefined) {
+      getToken();
+    }
+  }, [router.query]);
+
+  return (
+    <Container>
+      {userInfo?.id ? (
+        <>
+          <MainText>
+            반가워요
+            <br />
+            {userInfo?.nickname}님
+          </MainText>
+          <SemiText>같이가치를 위한 나눔을 시작해볼까요?</SemiText>
+          <RegisterButton>
+            <ButtonTextBlue>물품 등록하기</ButtonTextBlue>
+            <ButtonIcon>
+              <Image src={ChevronRightBlue} alt="/" width={24} height={24} />
+            </ButtonIcon>
+          </RegisterButton>
+          <ViewButton>
+            <ButtonTextOrange>둘러보기</ButtonTextOrange>
+            <ButtonIcon>
+              <Image src={ChevronRightOrange} alt="/" width={24} height={24} />
+            </ButtonIcon>
+          </ViewButton>
+        </>
+      ) : (
+        <LottieContainer>
+          <Lottie
+            options={{
+              animationData: animation,
+              loop: true,
+            }}
+          />
+        </LottieContainer>
+      )}
+    </Container>
+  );
+};
+
+export default Home;
