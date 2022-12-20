be11072d935dab430658d2ee27bf5f7b6df5f96e
sjoleee <cent7425@gmail.com>
2022-12-21 00:09:17 +0900

    fix: 불필요한 의존성 제거 및 오류 수정

--- src/atom.ts
+++
@@ -1,8 +0,0 @@
-import { atom } from 'recoil';
-
-import { MyDataProps } from '../pages/mypage';
-
-export const myDataState = atom({
-  key: 'myDataState',
-  default: { completedItems: [], notCompletedItems: [] } as MyDataProps,
-});
