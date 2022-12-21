bea8a3e920d3e4f4a93785cc3390ab4b0aba818f
sjoleee <cent7425@gmail.com>
2022-12-21 14:00:17 +0900

    fix: 모바일 100vh 주소창 높이 대응 코드 작성

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import styled, { ThemeProvider } from 'styled-components';
 import '../src/font/font.css';
 import type { AppProps } from 'next/app';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import GlobalStyle from '../src/styles/globalStyle';
 import { theme } from '../src/styles/theme';
@@ -13,7 +13,7 @@ const BodyInner = styled.div`
   // flex-direction: column;
   justify-content: center;
   // align-items: center;
-  min-height: 100vh;
+  min-height: calc(var(--vh, 1vh) * 100);
   background-color: ${(props) => props.theme.colors.gray100};
   background-image: url('https://firebasestorage.googleapis.com/v0/b/usemine-6a464.appspot.com/o/Asset%201%404x%201.png?alt=media&token=68325147-1a92-4b62-8cdd-32a1df96db63');
   // background-image: url("https://firebasestorage.googleapis.com/v0/b/usemine-6a464.appspot.com/o/bright-wall.png?alt=media&token=e303e287-dfde-4d60-a422-809bf322c5dc");
@@ -39,6 +39,15 @@ declare global {
 }
 
 const App = ({ Component, pageProps }: AppProps) => {
+  useEffect(() => {
+    function setScreenSize() {
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    }
+
+    window.addEventListener('resize', () => setScreenSize());
+  }, []);
+
   function kakaoInit() {
     // 페이지가 로드되면 실행
     window.Kakao.init(process.env.NEXT_PUBLIC_KAKAO_JS_API_KEY);
